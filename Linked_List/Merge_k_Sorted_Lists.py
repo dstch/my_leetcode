850fe1b9b44d6d172d63dac67ca27cc85d69108a
dstch <dstch@163.com>
2019-01-14 23:29:51 +0800

    accepted

--- Linked_List/Merge_k_Sorted_Lists.py
+++ Linked_List/Merge_k_Sorted_Lists.py
@@ -27,8 +27,44 @@ class ListNode:
 
 
 class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result = ListNode(0)
+        return_result = result
+        while l1 != None and l2 != None:
+            if l1.val < l2.val:
+                result.next = l1
+                l1 = l1.next
+            else:
+                result.next = l2
+                l2 = l2.next
+            result = result.next
+        if l1 == None:
+            result.next = l2
+        else:
+            result.next = l1
+        return return_result.next
+
     def mergeKLists(self, lists):
         """
         :type lists: List[ListNode]
         :rtype: ListNode
         """
+        temp_list = []
+        for node in lists:
+            if len(temp_list) < 2:
+                temp_list.append(node)
+            else:
+                temp_node = self.mergeTwoLists(temp_list[0], temp_list[1])
+                temp_list.clear()
+                temp_list.append(temp_node)
+                temp_list.append(node)
+        if len(lists) < 1:
+            return None
+        elif len(lists) == 1:
+            return lists[0]
+        return self.mergeTwoLists(temp_list[0], temp_list[1])
