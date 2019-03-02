ba516b18be63fd8992266f05a3e6b4f3c701843a
dstch <dstch@163.com>
2019-03-02 08:17:42 +0800

    add new question

---
+++ Linked_List/Remove_Duplicates_from_Sorted_List_II.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: dstch
+@license: (C) Copyright 2013-2019, Regulus Tech.
+@contact: dstch@163.com
+@file: Remove_Duplicates_from_Sorted_List_II.py
+@time: 2019/3/2 8:13
+@desc: Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+Example 1:
+
+Input: 1->2->3->3->4->4->5
+Output: 1->2->5
+
+Example 2:
+
+Input: 1->1->1->2->3
+Output: 2->3
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        cur = head
+        prev = cur
+        while cur is not None:
+            if cur.val == cur.next.val:
+                pass
+            else:
+                cur = cur.next
