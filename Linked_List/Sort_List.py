e64612aa62109926de2d6e1011a35775c4973bd7
dstch <dstch@163.com>
2019-04-09 23:01:01 +0800

    add merge sort method

--- Linked_List/Sort_List.py
+++ Linked_List/Sort_List.py
@@ -7,6 +7,7 @@
 @file: Sort_List.py
 @time: 2019/4/1 19:16
 @desc: Sort a linked list in O(n log n) time using constant space complexity.
+https://www.cnblogs.com/TenosDoIt/p/3666585.html
 """
 from util import ListNode
 
@@ -35,8 +36,53 @@ class Solution(object):
             cur = pre.next
         return temp_head.next
 
+    def sortList2(self, head):
+        """
+        merge sort
+        :return:
+        """
+
+        def find_mid(head, end):
+            slow = head
+            fast = head
+            while fast.next is not None and fast.next != end:
+                slow = slow.next
+                fast = fast.next.next
+            return slow
+
+        def merge_array(first, last):
+            temp_head = ListNode(-1)
+            if first.val > last.val:
+                temp_head.next = last
+            else:
+                temp_head.next = first
+                first = last
+            tmp = temp_head.next
+            while first is not None and tmp is not None:
+                if tmp.val > first.val:
+                    t = first.next
+                    first.next = tmp.next
+                    tmp.next = first
+                    tmp = tmp.next.next
+                    first = t
+                else:
+                    tmp = tmp.next
+            if first is not None:
+                tmp.next = first
+            return temp_head.next
+
+        def merge_sort(head, end):
+            if head != end:
+                mid = find_mid(head, end)
+                merge_sort(head, mid)
+                merge_sort(mid.next, end)
+                merge_array(head, end)
+            return head
+
+        return merge_sort(head, None)
+
 
 if __name__ == '__main__':
     head = ListNode.build_linked_list([-1, 5, 3, 4, 0])
     s = Solution()
-    print(ListNode.show_linked_list(s.sortList(head)))
+    print(ListNode.show_linked_list(s.sortList2(head)))
