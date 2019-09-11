61ed3cd9e5acb08bd9aa6ddfb2a65a8e540efdd9
dstch <dstch@163.com>
2019-09-11 23:05:29 +0800

    Runtime: 208 ms, faster than 41.18% of Python3 online submissions for Implement Trie (Prefix Tree). Memory Usage: 32.8 MB, less than 7.41% of Python3 online submissions for Implement Trie (Prefix Tree).

---
+++ Tree/Implement Trie (Prefix Tree).py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: dstch
+@license: (C) Copyright 2013-2019, Regulus Tech.
+@contact: dstch@163.com
+@file: Implement Trie (Prefix Tree).py
+@time: 2019/9/11 22:43
+@desc: Implement a trie with insert, search, and startsWith methods.
+
+Example:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");
+trie.search("app");     // returns true
+
+"""
+
+
+class TrieNode(object):
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.is_word = False
+
+
+class Trie(object):
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        curr = self.root
+        for ch in word:
+            curr = curr.children[ch]
+        curr.is_word = True
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        curr = self.root
+        for ch in word:
+            curr = curr.children.get(ch)
+            if curr is None:
+                return False
+        return curr.is_word
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        curr = self.root
+        for ch in prefix:
+            curr = curr.children.get(ch)
+            if curr is None:
+                return False
+        return True
+
+
+        # Your Trie object will be instantiated and called as such:
+        # obj = Trie()
+        # obj.insert(word)
+        # param_2 = obj.search(word)
+        # param_3 = obj.startsWith(prefix)
