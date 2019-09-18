249320559e9b282de9a89b558efdab1ab2090a5b
dstch <dstch@163.com>
2019-09-18 16:18:49 +0800

    Runtime: 76 ms, faster than 87.92% of Python online submissions for Course Schedule. Memory Usage: 14.6 MB, less than 45.76% of Python online submissions for Course Schedule.

---
+++ Graph/Course Schedule.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: dstch
+@license: (C) Copyright 2013-2019, Regulus Tech.
+@contact: dstch@163.com
+@file: Course Schedule.py
+@time: 2019/9/12 17:22
+@desc: There are a total of n courses you have to take, labeled from 0 to n-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+Example 1:
+
+Input: 2, [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take.
+             To take course 1 you should have finished course 0. So it is possible.
+
+Example 2:
+
+Input: 2, [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take.
+             To take course 1 you should have finished course 0, and to take course 0 you should
+             also have finished course 1. So it is impossible.
+
+"""
+import collections
+
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        graph = collections.defaultdict(list)
+        for u, v in prerequisites:
+            graph[u].append(v)
+        visited = [0] * numCourses
+        for i in range(numCourses):
+            if not self.dfs(graph, visited, i):
+                return False
+        return True
+
+    def dfs(self, graph, visited, i):
+        if visited[i] == 1:
+            return False
+        if visited[i] == 2:
+            return True
+        visited[i] = 1
+        for j in graph[i]:
+            if not self.dfs(graph, visited, j):
+                return False
+        visited[i] = 2
+        return True
+
+
+if __name__ == '__main__':
+    s = Solution()
+    s.canFinish(2, [[0, 1]])
