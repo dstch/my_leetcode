09e0fd83afca5a15d50cdcc82433e820206a4656
dstch <dstch@163.com>
2019-09-19 23:13:57 +0800

    add new question

---
+++ Dynamic Programming/Minimum Path Sum.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: dstch
+@license: (C) Copyright 2013-2019, Regulus Tech.
+@contact: dstch@163.com
+@file: Minimum Path Sum.py
+@time: 2019/9/19 22:45
+@desc: Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+Example:
+
+Input:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+Output: 7
+Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+
+
+"""
+
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+
+    def deep(self,grid,i,j):
