d541967e9692a10bfc7f620d21949f96c79a7e49
YvetteQian <qianya_1990@163.com>
2021-02-16 22:53:56 +0800

    Accepted 195/195 cases passed (36 ms) Your runtime beats 59.8 % of python3 submissions Your memory usage beats 13.21 % of python3 submissions (14.5 MB)

--- Tree/101.symmetric-tree.py
+++ Tree/101.symmetric-tree.py
@@ -1,3 +1,11 @@
+'''
+Author: your name
+Date: 2021-02-14 23:24:28
+LastEditTime: 2021-02-14 23:46:59
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \my_leetcode\Tree\101.symmetric-tree.py
+'''
 #
 # @lc app=leetcode id=101 lang=python3
 #
@@ -15,23 +23,16 @@
 
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        def func(node, result):
-            if node is not None:
-
-                func(node.left, result)
-                result.append(node.val)
-                func(node.right, result)
-
-        def func1(node, result):
-            if node is not None:
-                func1(node.right, result)
-                result.append(node.val)
-                func1(node.left, result)
-                
-                
-        left, right = [], []
-        func(root.left, left)
-        func1(root.right, right)
-        return left == right
+        def func(node1, node2):
+            if node1 is None and node2 is None:
+                return True
+            if node1 is None or node2 is None:
+                return False
+            if node1.val != node2.val:
+                return False
+            return func(node1.left, node2.right) and func(node1.right, node2.left)
+        if root is None:
+            return True
+        return func(root.left, root.right)
 
 # @lc code=end
