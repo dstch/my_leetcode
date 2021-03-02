dd5012469ec6bc707d0a78c20d5543fa4fe41c52
dstch <dstch@163.com>
2021-03-03 00:17:10 +0800

    Runtime: 32 ms, faster than 62.66% of Python3 online submissions for Binary Tree Inorder Traversal. Memory Usage: 14.1 MB, less than 78.00% of Python3 online submissions for Binary Tree Inorder Traversal.

--- Tree/94.binary-tree-inorder-traversal.py
+++ Tree/94.binary-tree-inorder-traversal.py
@@ -20,8 +20,33 @@ class Solution:
                 func(node.left, result_list)
                 result_list.append(node.val)
                 func(node.right, result_list)
+
         result = []
         func(root, result)
         return result
+
+    def inorderTraversal2(self, root):
+        """
+        非递归写法
+        :param root:
+        :return:
+        """
+        if root is None:
+            return None
+        stack = []
+        res = []
+        while root:
+            stack.append(root)
+            root = root.left
+        while len(stack) != 0 or root is not None:
+            node = stack[-1]
+            stack.pop()
+            res.append(node.val)
+            if node.right is not None:
+                node = node.right
+                while node:
+                    stack.append(node)
+                    node = node.left
+        return res
 
 # @lc code=end
