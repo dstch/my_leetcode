c8906562812aaf170cbb29a83243b6fc7c1c5eae
dstch <dstch@163.com>
2021-04-07 23:51:06 +0800

    执行耗时:864 ms,击败了5.00% 的Python3用户 		内存消耗:18.1 MB,击败了22.25% 的Python3用户

---
+++ Tree/[687]Longest Univalue Path.py
@@ -0,0 +1,53 @@
+# Given the root of a binary tree, return the length of the longest path, where 
+# each node in the path has the same value. This path may or may not pass through 
+# the root. 
+# 
+#  The length of the path between two nodes is represented by the number of edge
+# s between them. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: root = [5,4,5,1,1,5]
+# Output: 2
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: root = [1,4,5,4,4,5]
+# Output: 2
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  The number of nodes in the tree is in the range [0, 104]. 
+#  -1000 <= Node.val <= 1000 
+#  The depth of the tree will not exceed 1000. 
+#  
+#  Related Topics Tree Recursion 
+#  👍 2239 👎 554
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def longestUnivaluePath(self, root: TreeNode) -> int:
+        def func(node, value):
+            if not node or node.val != value:
+                return 0
+            return 1 + max(func(node.left, node.val), func(node.right, node.val))
+
+        if not root:
+            return 0
+        sub = max(self.longestUnivaluePath(root.left), self.longestUnivaluePath(root.right))
+        return max(sub, func(root.left, root.val) + func(root.right, root.val))
+# leetcode submit region end(Prohibit modification and deletion)
