c7aeb36e2c2f3e70e25ed0d9d7c42f8865b32737
dstch <dstch@163.com>
2021-05-11 23:18:13 +0800

    解答成功: 		执行耗时:808 ms,击败了18.88% 的Python3用户 		内存消耗:24.5 MB,击败了27.57% 的Python3用户

--- Dynamic Programming/[1143]Longest Common Subsequence.py
+++ Dynamic Programming/[1143]Longest Common Subsequence.py
@@ -52,9 +52,14 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         mono = []
-        for i in range(len(text1)):
+        for i in range(len(text1)):  # 这里需要注意，可能会出现浅拷贝问题
             mono.append([-1] * len(text2))
 
+        # 思考：“最长”之类的可以思考使用动态规划
+        # 以dp来动态思考，dp[i][j]代表t1[i:]与t2[j:]匹配的最长子串
+        # 所以我们要求的是dp[0][0]
+        # 进而思考到使用递归，将子问题提出来：dp[i][j]=max(dp[i+1][j],dp[i][j+1])
+        # base case就是i,j各等于字符串长度，即无匹配，为0
         def dp(t1, i, t2, j):
             if i == len(t1) or j == len(t2):
                 return 0
